318cdb1a713a30659707cf0dc43f8386c5eab801
pcasano <pablo.casanovas@pm.me>
2024-04-07 11:24:01 +0200

    added css for calendar

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -8,23 +8,35 @@ import { Component, OnInit } from '@angular/core';
 export class CalendarComponent implements OnInit{
 
   daysOfWeek: string[] = ['M', 'T', 'W', 'T', 'F', 'S', 'S'];
-  numbers: number[] = [];
+  monthDaysArray: number[] = [];
+  firstDayOfWeek: number;
 
   ngOnInit(): void {
     const currentDate = new Date();
     // Get year and month
     const year = currentDate.getFullYear();
-    const month = currentDate.getMonth() + 1; // Month is zero-based, so add 1
+    // const month = currentDate.getMonth() + 1;
+    const month = 6;
+
     // Create a new date object for the first day of next month
     const firstDayOfNextMonth = new Date(year, month, 1);
     // Subtract 1 day from the first day of next month to get the last day of the current month
     const lastDayOfMonth = new Date(firstDayOfNextMonth.getTime() - 1);
     // Get the day of the month from the last day of the current month
     const numberOfDaysInMonth = lastDayOfMonth.getDate();
- 
+
+    const numberOfTheWeek = this.getFirstDayOfWeek(year, month);
+    for (let i = 1; i <= numberOfTheWeek; i++) {
+      this.monthDaysArray.push(0);
+    }
     for (let i = 1; i <= numberOfDaysInMonth; i++) {
-      this.numbers.push(i);
+      this.monthDaysArray.push(i);
     }
   }
+
+  getFirstDayOfWeek(year: number, month: number): number {
+    const firstDayOfMonth = new Date(year, month - 1, 1);
+    return  firstDayOfMonth.getDay() - 1
+}
 
 }
