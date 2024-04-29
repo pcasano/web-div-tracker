8ec3c9b002076e6c631f727859a240315377b490
pcasano <pablo.casanovas@pm.me>
2024-04-29 16:20:30 +0200

    use of component store

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { CompanyStore } from '../store/company-store';
 import { Company } from '../store/models/company.model';
 import { ContentObserver } from '@angular/cdk/observers';
+import { HttpErrorResponse } from '@angular/common/http';
 
 
 @Component({
@@ -19,8 +20,11 @@ export class CalendarComponent implements OnInit{
 
   constructor(private readonly companyStore: CompanyStore) {}
 
-  companies: Company[] = [];
-  companiesToDisplay: Company[] = [];
+  companies: Company[] | undefined | null = [];
+  companyInProgress: boolean;
+  companyError: HttpErrorResponse | undefined | null = null;
+
+  companiesToDisplay: Company[] | undefined | null = [];
   selectedDay: number;
 
   isCompanyHovered: boolean = false;
@@ -50,6 +54,9 @@ export class CalendarComponent implements OnInit{
     for (let i = 1; i <= numberOfDaysInMonth; i++) {
       this.monthDaysArray.push(i);
     }
+    this.companyStore.companies$.subscribe(cs => this.companies = cs);
+    this.companyStore.companyInProgress$.subscribe(progress => this.companyInProgress = progress);
+    this.companyStore.companyError$.subscribe(error => this.companyError = error);
 
     }
 
@@ -58,29 +65,29 @@ export class CalendarComponent implements OnInit{
     return  firstDayOfMonth.getDay() - 1
 }
 
-  getDividendsDayForCalendar(givenDay: number): Company[] {
-    return this.companies.filter(
+  getDividendsDayForCalendar(givenDay: number): Company[] | undefined | null {
+    return this.companies?.filter(
       dividend => this.isInCurrentMonth(dividend.dividendPaymentDate) && this.isSameDay(dividend.dividendPaymentDate, givenDay));
   }
 
   showDividendsGivenDay(givenDay: number) {
     this.companiesToDisplay = [];
     this.companiesToDisplay = this.getDividendsDayForCalendar(givenDay);
-    this.companiesToDisplay.forEach(company => console.log(company));
+    this.companiesToDisplay?.forEach(company => console.log(company));
     this.selectedDay = givenDay;
   }
 
   showDividendsCurrentMonth() {
     this.companiesToDisplay = [];
-    this.companiesToDisplay = this.companies.filter(
+    this.companiesToDisplay = this.companies?.filter(
       dividend => this.isInCurrentMonth(dividend.dividendPaymentDate));
-      this.companiesToDisplay.forEach(company => console.log(company));
+      this.companiesToDisplay?.forEach(company => console.log(company));
   }
 
   showFullPortfolio() {
     this.companiesToDisplay = [];
     this.companiesToDisplay = this.companies;
-    this.companies.forEach(company => console.log(company));
+    this.companies?.forEach(company => console.log(company));
   }
 
   isInCurrentMonth(dateString: string): boolean {
--- src/app/store/company-store.ts
+++ src/app/store/company-store.ts
@@ -2,70 +2,53 @@ import { Injectable } from "@angular/core";
 import { ComponentStore, tapResponse } from "@ngrx/component-store";
 import { CompanyService } from "./services/company.service";
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
-import { Observable, Observer, pipe, switchMap, tap } from "rxjs";
 import { Company } from "./models/company.model";
 
 
 export interface CompanyState {
   companies: Company[] | undefined | null;
-  // companyInProgress: boolean;
-  // companyError: HttpErrorResponse | undefined | null
+  companyInProgress: boolean;
+  companyError: HttpErrorResponse | undefined | null
 }
 
-// export const initCompanyState: CompanyState = {
-//   companies: null,
-//   companyInProgress: true,
-//   companyError: null
-// }
+export const initCompanyState: CompanyState = {
+  companies: null,
+  companyInProgress: true,
+  companyError: null
+}
 
   @Injectable()
   export class CompanyStore extends ComponentStore<CompanyState> {
 
     constructor(private companyService: CompanyService) {
-      super({companies: []});
+      super(initCompanyState);
     }
 
     readonly companies$ = this.select((state) => state.companies);
+    readonly companyInProgress$ = this.select((state) => state.companyInProgress);
+    readonly companyError$ = this.select((state) => state.companyError);
 
-    private setCompanies = this.updater((state, companies: Company[]) => ({
+    private updateCompanies = this.updater((state: CompanyState, companies: Company[]) => ({
       ...state,
-      companies
+      companies: companies.map(company => ({...company, imagePath:"../../assets/images/" + company.id + ".png"})),
+      companyInProgress: false,
+      companyError: null
+    }))
+
+    private updateCompaniesError = this.updater((state: CompanyState, error: HttpErrorResponse) => ({
+      ...state,
+      companies: null,
+      companyInProgress: false,
+      companyError: error
     }));
 
-    readonly fetchCompanies = this.effect(() => {
-      return this.companyService.getCompanies().pipe(tap((companies) => this.setCompanies(companies)))
+    readonly getCompanies = this.effect(() => {
+
+      return this.companyService.getCompanies().pipe(
+        tapResponse<Company[], HttpErrorResponse>(
+        (companies: Company[]) => this.updateCompanies(companies),
+        (error: HttpErrorResponse) => this.updateCompaniesError(error)
+      ))
     });
-
-
-
-
-    // readonly selectCompanies$: Observable<Company[] | undefined | null> = this.select((state: CompanyState) => state.companies);
-    // readonly selectCompanyInProgress: Observable<boolean> = this.select((state: CompanyState) => state.companyInProgress);
-    // readonly selectCompanyError$: Observable<HttpErrorResponse | undefined | null> = this.select((state: CompanyState) => state.companyError);
-
-
-
-
-
-
-
-
-    // getCompanyEffect = this.effect(
-    //   this.companyService.getCompanies().pipe(
-    //     tapResponse<Company , HttpErrorResponse>(
-    //       (company: Company) => console.log(""),
-    //       (error: HttpErrorResponse) => console.log("");
-    //     ))
-    // )
-
-    // readonly company$ = this.select(state => state.company);
-
-    // fetchCompany(): void {
-    //   this.companyService.fetchCompany().subscribe(company => {
-    //     this.setState({ company });
-    //   });
-    // }
-
-
 
   }
